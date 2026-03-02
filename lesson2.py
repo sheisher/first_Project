f260d500eeccab0b55ac9c0104b7a21d40d32405
sheisher <vannessaaddai@gmail.com>
2026-03-02 09:28:58 -1200

    my first python lesson

---
+++ lesson2.py
@@ -0,0 +1,7 @@
+a = "My name is "
+name = input('what is your name ?')
+total = 0
+for letter in name:
+ total += (letter)
+number = "your number is :"
+print(a,name,number,total)
---
+++ lesson3.py
@@ -0,0 +1,8 @@
+color = input("What are the colors of the traffic light? ")
+if color == "red":
+    print("Stop")
+elif color == "yellow":
+    print("Get ready")
+elif color == "green":
+    print("Go")
+else:    print("Invalid color")

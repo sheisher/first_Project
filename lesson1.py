50cd4167fbcac21bb99d8a5e041aebf9e1ce97a3
sheisher <vannessaaddai@gmail.com>
2026-03-02 09:23:14 -1200

    my first python lesson

---
+++ lesson1.py
@@ -0,0 +1,11 @@
+y = " my name is "
+z = input('what is your name :')
+a = "my age is "
+b = input('what is your age :')
+c = " i study at "
+d = input('which school do you study in :')
+g = "I live at "
+e = input('where do you live :')
+h = "i was born in "
+f = input('which year were you born :')
+print(y+z,a+b,c+d,g+e,h+f)

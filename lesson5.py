8a1fa5dd33752f1b446d217cdaaf3dc8dd31864a
sheisher <vannessaaddai@gmail.com>
2026-03-04 09:29:22 -1200

    upgrade on lesson

---
+++ lesson5.py
@@ -0,0 +1,6 @@
+# This program calculates the area of a triangle
+base = float(input(("what is the base?")))
+height = float(input("what is the height?"))
+
+area = 0.5 * (base) *(height)
+print ("The area of the triangle is:", area)
---
+++ lesson6.py
@@ -0,0 +1,7 @@
+num1 =float(input("Enter the first number: "))
+num2 =float(input("Enter the second number: "))
+addition = (num1) + (num2)
+division = (num1) / (num2)
+
+print ("The sum of the two numbers is:", addition)
+print ("The division of the two numbers is:", division)
